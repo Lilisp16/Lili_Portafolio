89608c1869bc70fdc53d6bc4917ff59186a4f4cb
Lilisp16 <lijhosa@gmail.com>
2025-12-14 22:10:32 -0500

    Preparar proyecto para GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
+  base: '/Lili_Portafolio/', // <- esto es clave para GitHub Pages
   plugins: [react()],
   server: {
     port: 5173
