08d7db116c650b67272751d15904b140e721ad07
Lilisp16 <lijhosa@gmail.com>
2025-12-16 08:41:26 -0500

    Nuevo deploy limpio

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
-  base: '/Lili_Portafolio/', // <- esto es clave para GitHub Pages
+  base: '/Lili_Portafolio/',
   plugins: [react()],
   server: {
     port: 5173
